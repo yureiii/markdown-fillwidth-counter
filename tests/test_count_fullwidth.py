bc34c98ad78ae24c50c8064bb38fc6271a036f8c
yureiii <32612544+yureiii@users.noreply.github.com>
2023-12-21 13:45:30 +0900

    add .gitignore add test case

--- tests/test_count_fullwidth.py
+++ tests/test_count_fullwidth.py
@@ -8,3 +8,14 @@ def test_count_fullwidth_text():
     assert count_fullwidth_text("<ruby>漢字<rt>かんじ</rt></ruby>") == 2
     assert count_fullwidth_text("# 見出し\n 本文") == 2
     assert count_fullwidth_text("<!-- コメントアウトした文字は数えない -->") == 0
+    text = """# サンプル文章
+これはテスト<ruby>用<rt>よう</rt></ruby>の文章です．43 文字になることを想定しています．
+全角の文字は数えます．（　）
+
+<!-- コメントアウトした文字は数えない -->
+
+## 記号
+! @ # $ % ^ & * ( ) - _ = + [ ] { } \\ | ; : ' " , < . > / ? ` ~
+"""
+
+    assert count_fullwidth_text(text) == 43
